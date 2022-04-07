ff28443df0c402dc1298442b27def22147fb498d
odeadglaz <oded.goldglas+git@fiverr.com>
2022-04-07 13:58:40 +0300

    More.

--- src/index.js
+++ src/index.js
@@ -84,7 +84,7 @@ class ReadinessManager {
         readyState = false;
 
         Object.values(this[Symbols.beacons])
-            .map((beacon) => this[Symbols.execute](beacon));
+            .forEach((beacon) => this[Symbols.execute](beacon));
 
         return this;
     }
